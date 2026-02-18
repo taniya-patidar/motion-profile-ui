d34c9fb0cab66f30089c617a82bd56d70c6a94dd
taniya-patidar <gitgithub77@gmail.com>
2026-02-18 21:59:49 +0530

    change photo

--- code/src/App.jsx
+++ code/src/App.jsx
@@ -3,7 +3,7 @@ import ProfileCard from "./components/ProfileCard";
 import { AnimatePresence, motion } from "framer-motion";
 import "./App.css";
 import girl1 from "./assets/Girl.webp";
-import girl2 from "./assets/Girl1.webp";
+import girl2 from "./assets/Girl1.avif";
 // import girl3 from "./assets/Girl3.webp";
 // import girl4 from "./assets/Girl4.webp";
 // import girl5 from "./assets/Girl5.webp";
